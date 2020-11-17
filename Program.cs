1277efb553075b34cf29eff1cfeae284f28b81ca
Parker Sullivan <parkersullivan77@gmail.com>
2020-11-17 02:30:22 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using PizzaProblems.Data;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PizzaProblems
+{
+     public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //1. Get the IWebHost which will host this application.
+            var host = CreateWebHostBuilder(args).Build();
+
+            //2. Find the service layer within our scope.
+            using (var scope = host.Services.CreateScope())
+            {
+                //3. Get the instance of BoardGamesDBContext in our services layer
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<PizzaContext>();
+
+                //4. Call the DataGenerator to create sample data
+                dataGenerator.Initialize(services);
+            }
+
+            //Continue to run the application
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using PizzaProblems.Data;
+
+
+namespace PizzaProblems
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<PizzaContext>(opt => opt.UseSqlServer
+                (Configuration.GetConnectionString("PizzaConnection")));
+            
+            services.AddControllers();
+
+            services.AddScoped<IPizzaRepo,DatabasesRepo>();
+            //services.AddDbContext<PizzaContext>(options => options.UseInMemoryDatabase(databaseName: "Pizzas"));
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
